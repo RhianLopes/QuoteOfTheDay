0b26ed43552caeb9b0ae04bc45c4344cbf0216d0
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-07-20 19:56:58 -0300

    feat: introduce base project

---
+++ QuoteOfTheDay/QuoteOfTheDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuoteOfTheDay
+//
+//  Created by Rhian Lopes da Costa on 20/07/20.
+//  Copyright © 2020 Rhian Lopes da Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
